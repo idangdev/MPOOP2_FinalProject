6fed622aec001897a6cc7258c2d37c7e3dd07fdb
idangdev <idang.dev@gmail.com>
2020-12-02 13:47:17 +0700

    test

--- app/src/main/java/com/wildan/mpoop2_finalproject/MainActivity.kt
+++ app/src/main/java/com/wildan/mpoop2_finalproject/MainActivity.kt
@@ -2,7 +2,7 @@ package com.wildan.mpoop2_finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
