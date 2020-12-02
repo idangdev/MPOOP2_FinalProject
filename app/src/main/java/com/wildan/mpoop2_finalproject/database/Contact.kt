8c7447be8f30f1640a44e7988d6d4fee3fafce42
Hadi Kusumanto <56163029+hadi-web@users.noreply.github.com>
2020-12-02 15:11:52 +0700

    test

--- app/src/main/java/com/wildan/mpoop2_finalproject/database/Contact.kt
+++ app/src/main/java/com/wildan/mpoop2_finalproject/database/Contact.kt
@@ -2,7 +2,7 @@ package com.wildan.mpoop2_finalproject.database
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+// test
 @Entity
 data class Contact(
     @PrimaryKey(autoGenerate = true) var id: Int,
