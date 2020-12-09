a9e49daf4d945a29d10cc0ccca19f0d6b864da95
idangdev <idang.dev@gmail.com>
2020-12-09 14:19:57 +0700

    Menu Contact

---
+++ app/src/main/java/com/wildan/mpoop2_finalproject/ContactActivity.kt
@@ -0,0 +1,13 @@
+package com.wildan.mpoop2_finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+
+
+    }
+}
