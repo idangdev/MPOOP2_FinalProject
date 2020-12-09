4eb92f0566652f63f4c7b70fbd8fd8291e7e17a2
idangdev <idang.dev@gmail.com>
2020-12-09 14:11:02 +0700

    test

--- app/src/main/java/com/wildan/mpoop2_finalproject/MainActivity.kt
+++ app/src/main/java/com/wildan/mpoop2_finalproject/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.wildan.mpoop2_finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.wildan.mpoop2_finalproject.databinding.ActivityMainBinding
+
 //test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.contactBtn.setOnClickListener{
+            val intent = Intent(this, ContactActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
