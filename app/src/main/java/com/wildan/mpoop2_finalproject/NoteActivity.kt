a56869d2bdb572ac5b10eb5b40682ddc6fa8a963
Hadi Kusumanto <56163029+hadi-web@users.noreply.github.com>
2020-12-23 10:14:01 +0700

    Note: TroubleShooting & add delete method CRUD

--- app/src/main/java/com/wildan/mpoop2_finalproject/NoteActivity.kt
+++ app/src/main/java/com/wildan/mpoop2_finalproject/NoteActivity.kt
@@ -1,11 +1,58 @@
 package com.wildan.mpoop2_finalproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.wildan.mpoop2_finalproject.database.NoteRoomDatabase
+import com.wildan.mpoop2_finalproject.model.Note
+import kotlinx.android.synthetic.main.activity_note.*
 
 class NoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
+
+        getNotesData()
+
+        floatingActionButton.setOnClickListener {
+            startActivity(Intent(this, EditActivity::class.java))
+        }
+    }
+
+    private fun getNotesData(){
+        val database = NoteRoomDatabase.getDatabase(applicationContext)
+        val dao = database.getNoteDao()
+        val listItems = arrayListOf<Note>()
+        listItems.addAll(dao.getAll())
+        setupRecyclerView(listItems)
+        if (listItems.isNotEmpty()){
+            text_view_note_empty.visibility = View.GONE
+        }
+        else{
+            text_view_note_empty.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setupRecyclerView(listItems: ArrayList<Note>){
+        recycler_view_main.apply {
+            adapter = NoteAdapter(listItems, object : NoteAdapter.NoteListener{
+                override fun OnItemClicked(note: Note) {
+                    val intent = Intent(this@NoteActivity, EditActivity::class.java)
+                    intent.putExtra(EditActivity().EDIT_NOTE_EXTRA, note)
+                    startActivity(intent)
+                }
+            })
+
+            layoutManager = LinearLayoutManager(this@NoteActivity)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getNotesData()
     }
 }
+
+
